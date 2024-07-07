3fb948b3a1766f850c4d8c85d3b62eb818e84c17
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-07-07 23:24:49 +0530

    go stack implementation

--- Basics.go
+++ Basics.go
@@ -1,21 +1,21 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var n int
-	fmt.Println("Enter the size of the array:")
-	fmt.Scan(&n)
-	list := make([]string, n)
+// func main() {
+// 	var n int
+// 	fmt.Println("Enter the size of the array:")
+// 	fmt.Scan(&n)
+// 	list := make([]string, n)
 
-	for i:=0; i<n;i++ {
-		fmt.Scan(&list[i])
-	}
+// 	for i:=0; i<n;i++ {
+// 		fmt.Scan(&list[i])
+// 	}
 
-	for i, val := range list {
-		fmt.Println("The value at "+string(rune(i))+"is "+val)
-	}
-}
+// 	for i, val := range list {
+// 		fmt.Println("The value at "+string(rune(i))+"is "+val)
+// 	}
+// }
 
---
+++ Stack.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stack []int
+
+func (st *Stack) Push(val int) {
+	*st = append(*st, val)
+}
+
+func (st *Stack) Pop() int {
+	if st.IsEmpty() {
+		return -1
+	}
+
+	val := (*st)[len(*st)-1]
+	*st = (*st)[0:len(*st)-1]
+	return val
+}
+
+func (st *Stack) IsEmpty() bool {
+	return len(*st) == 0
+}
+
+func main() {
+	stack := Stack{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+}
