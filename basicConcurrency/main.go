5f92b5453a69e66a57a610ba01a9aad46380f469
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-07-23 16:08:16 +0530

    concurrency channels

---
+++ basicConcurrency/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func testFunc(channel chan int) {
+	time.Sleep(time.Second * 2)
+	channel <- 69
+}
+
+func main() {
+	// ch := make(chan int)
+	// ch <- 69
+	// will wait until someone reads from the channel
+	// v := <-ch
+
+	channel := make(chan int)
+	go testFunc(channel)
+	value := <-channel
+	fmt.Println(value)
+}
