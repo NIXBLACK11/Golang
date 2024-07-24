7643cea7ef3a6f65cfab433c0e6291f8eabd67d2
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-07-24 13:08:37 +0530

    select

--- basicConcurrency/bufferedChan.go
+++ basicConcurrency/bufferedChan.go
@@ -8,6 +8,7 @@ import (
 func putEmails(channel chan string, emails []string) {
 	for _, email := range (emails) {
 		channel <- email
+		fmt.Println("email: "+email)
 	}
 	close(channel) // Close the channel after all emails are sent
 }
@@ -21,9 +22,46 @@ func sendEmails(channel chan string) {
 
 func main() {
 	emails := []string{"efef", "hello", "efwef", "hi", "bye"}
-
+	
 	channel := make(chan string, 5)
 	go putEmails(channel, emails)
 	
 	sendEmails(channel)
+
+	// To check if channel is still open
+	// _, ok := <-channel
+	
+	// if ok { 
+	// 	fmt.Println("Channel still open") 
+	// 	return
+	// }
+
+	// Select
+	emailChannel := make(chan string)
+	smsChannel := make(chan string)
+
+	go func() {
+		emailChannel<-"email"
+		close(emailChannel)
+	}()
+
+	go func() {
+		smsChannel<-"sms"
+		close(smsChannel)
+	}()
+
+	for {
+		select {
+		case email, ok := <-emailChannel:
+			if !ok {
+				return 
+			}
+			fmt.Println(email)
+		case sms, ok := <-smsChannel:
+			if !ok {
+				return
+			}
+			fmt.Println(sms)
+		}
+	}
 }
