3d74dd132cdca7abb1784637e7ca79375f2845c8
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-08-12 18:34:27 +0530

    channel buffering

--- goRoutines/main.go
+++ goRoutines/main.go
@@ -14,11 +14,31 @@ func asyncWork(message chan string) {
 	message<-"done"
 }
 
+func messageOperate(messages chan string, message string) {
+	time.Sleep(time.Second*1)
+	messages<-message
+	fmt.Println(message, " message sent!!")
+}
+
 func main() {
+	// Basics of channel
 	message:=make(chan string)
 
 	go asyncWork(message)
 
 	msg:=<-message
 	fmt.Println(msg)
+
+	// Buffered channels
+	messages := make(chan string, 2)
+	go messageOperate(messages, "First")
+	go messageOperate(messages, "Second")
+	go messageOperate(messages, "Third")
+	go messageOperate(messages, "Fourth")
+	go messageOperate(messages, "Fifth")
+	go messageOperate(messages, "Sixth")
+
+	fmt.Println(<-messages, " message received!!")
+	fmt.Println(<-messages, " message received!!")
+	fmt.Println(<-messages, " message received!!")
 }
